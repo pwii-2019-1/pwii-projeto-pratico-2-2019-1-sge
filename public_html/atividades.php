e119fb5fc2a37bb3cf8e70c7d93786fd8d2ea9d6
Ana Luiza Souza <31023081+gsanaluiza@users.noreply.github.com>
2019-06-08 21:06:34 -0300

    Update atividades.php

--- public_html/atividades.php
+++ public_html/atividades.php
@@ -22,9 +22,7 @@ $evento = $eventos->listarEvento($evento_id);
 $atividade = $atividades->listarAtividades($evento_id);
 $ativUsuario = $presencas->listarPresencas([$evento_id, Autenticacao::getCookieUsuario()], "atividades");
 $x = 0;
-echo "<pre>";
-print_r($ativUsuario);
-echo "</pre>";
+
 ?>
 
 <main role='main'>
